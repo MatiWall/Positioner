601f033d745ce08e42c2cb9051a14a60cc15a7b4
Matias Wallenius <MatiWall@live.dk>
2025-09-14 17:18:14 +0200

    gonna move browser control to node

--- Positioner/Handlers/BrowserHandlerV1.cs
+++ Positioner/Handlers/BrowserHandlerV1.cs
@@ -1,11 +1,8 @@
-﻿using Positioner.Manager;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using Positioner.Models;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+
 
 namespace Positioner.Serives
 {
@@ -31,47 +28,7 @@ namespace Positioner.Serives
 
 
             string url = spec.Urls[0];
-            var beforeWindows = WindowManager.GetAllWindowHandles();
 
-            Process.Start(new ProcessStartInfo
-                {
-                    FileName = "chrome.exe",
-                    Arguments = $"--new-window \"{url}\"",
-                    UseShellExecute = true // important for opening URLs
-                });
-
-            // Poll for the new window handle (up to 10s)
-            IntPtr? windowHandle = null;
-            for (int i = 0; i < 10; i++)
-            {
-                var afterWindows = WindowManager.GetAllWindowHandles().ToHashSet();
-                afterWindows.ExceptWith(beforeWindows);
-                windowHandle = afterWindows.FirstOrDefault();
-                if (windowHandle.HasValue) break;
-                Thread.Sleep(1000);
-            }
-
-            // Open the remaining URLs in the same browser (they will open in tabs)
-            for (int i = 1; i < spec.Urls.Count; i++)
-            {
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = "chrome.exe",
-                    Arguments = $"\"{spec.Urls[i]}\"",
-                    UseShellExecute = true
-                });
-            }
-
-            // Move and resize the new window
-            if (windowHandle.HasValue)
-            {
-                WindowManager.MoveWindowHandle(windowHandle.Value, pos.X, pos.Y, pos.Width, pos.Height);
-                Console.WriteLine($"Opened window for {entity.Metadata.Name} at ({pos.X},{pos.Y}) size {pos.Width}x{pos.Height}");
-            }
-            else
-            {
-                Console.WriteLine("Could not detect new browser window.");
-            }
 
         }
     }
